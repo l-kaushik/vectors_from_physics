a72d4448d0b6dc9212edc281f97518f5c79c12ac
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-08-22 01:11:58 +0530

    supportive functions

--- vectors.py
+++ vectors.py
@@ -1,3 +1,4 @@
+import math
 
 class Vector:
 
@@ -6,13 +7,53 @@ class Vector:
         self.j = j if j is not None else 0
         self.k = k if k is not None else 0
 
+    @classmethod
+    def from_magnitude(cls, magnitude, angle_xy, angle_xz=None) :
+        ''' this function takes magnitude and angle of xy plane and xz plane, then returns a vector with x, y, z coordinates'''
+        x = round(magnitude * math.cos(math.radians(angle_xy)), 2)
+        y = round(magnitude * math.sin(math.radians(angle_xy)), 2)
+
+        if angle_xz is not None:
+            z = round(magnitude * math.cos(math.radians(angle_xz)), 2)
+            return cls(x, y, z)
+        else:
+            return cls(x, y, 0)
+
+    @classmethod
+    def angle_between(cls, vector1, vector2):
+        product = vector1.dot_product(vector2)
+        magnitude_product = vector1.magnitude() * vector2.magnitude()
+
+        cos_angle = product / magnitude_product
+        angle_radians = math.acos(cos_angle)
+        angle_degrees = round(math.degrees(angle_radians), 2)
+
+        return angle_degrees
+
+    @classmethod
+    def check_negative(cls,first,other):
+        if(first.to_negative() == other):
+            return 1
+        else:
+            return 0
+        
+    @classmethod
+    def check_equal(cls,first,other):
+        if(first == other):
+            return 1
+        else:
+            return 0
+        
     def display(self):
         print(f"{self.i}i, {self.j}j, {self.k}k")
 
-    def dotProduct(self, other):
-        dot_product = self.i * other.i + self.j * other.j + self.k * other.k
+    def magnitude(self):
+        return math.sqrt(self.i ** 2 + self.j ** 2 + self.k ** 2)
 
-        return dot_product
+    def dot_product(self, other):
+        product = self.i * other.i + self.j * other.j + self.k * other.k
+
+        return product
 
     def crossProduct(self, other):
         new_i = self.j * other.k - self.k * other.j
@@ -20,6 +61,14 @@ class Vector:
         new_k = self.i * other.j - self.j * other.i
 
         return Vector(new_i, new_j, new_k)
+
+    def to_negative(self):
+        x = self.i * -1
+        y = self.j * -1
+        z = self.k * -1
+
+        return Vector(x, y, z)
+    
 
     def __add__(self, other):
         new_i = self.i + other.i
@@ -42,18 +91,15 @@ class Vector:
             new_k = self.k * other
 
             return Vector(new_i, new_j, new_k)
-
-        # elif isinstance(other, Vector):
-        #     dot_product = self.i * other.i + self.j * other.j + self.k * other.k
-
-        #     return dot_product
+        
+    def __eq__(self, other):
+        return self.i == other.i and self.j == other.j and self.k == other.k
 
 
 if __name__ == "__main__":
-    v1 = Vector(1, 2, 3)
-    v2 = Vector(3, 2, 1)
-    # v1 = v1 * 2
-    # v1.display()
-    v1 = v1.crossProduct(v2)
+    v1 = Vector().from_magnitude(5, 45,60)
 
-    v1.display()
+    v2 = v1.to_negative()
+    v1 = v1.to_negative()
+
+    print(Vector.check_negative(v1,v2))
