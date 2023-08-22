31bbe05fa1c03cebd13df13771909ff13a755bd9
l-kaushhik <contact.lokeshkaushik@gmail.com>
2023-08-22 21:56:39 +0530

    created display function to print vectors

--- vectors.cpp
+++ vectors.cpp
@@ -7,18 +7,27 @@ class Vector
     int x, y, z;
 
     public:
-        Vector(){}
+        // Vector(){}
         Vector(int, int,int);
+        void display();
 
 };
 
-Vector :: Vector(int i, int j, int k){
+Vector :: Vector(int i = 0, int j = 0, int k = 0){
     x = i;
     y = j;
     z = k;
 }
 
+void Vector :: display(){
+    cout<<x<<"i"<<", "<<y<<"j"<<", "<<z<<"k"<<endl;
+}
+
+
 int main()
 {
+    Vector a;
+    a = Vector(3,5,6);
+    a.display();
     return 0;
 }
